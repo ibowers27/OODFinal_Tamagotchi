d290bf43e46d421e1e1084f30f1aa7eabfdb8cf1
ibowers27 <i.bowers27@ncf.edu>
2024-12-03 14:08:24 -0500

    Tamagotchi Builder?

--- TamagotchiBuilder.java
+++ TamagotchiBuilder.java
@@ -1,3 +1,31 @@
 public class TamagotchiBuilder {
-	
+	private Tamagotchi tamagotchi;
+
+    // constructor for connecting the builder with the tamagotchi
+    public TamagotchiBuilder(Tamagotchi tamagotchi) {
+        this.tamagotchi = tamagotchi;
+    }
+
+    // building methods
+    public void buildColor(Color color) {
+        tamagotchi.setColor(color);
+    }
+
+    public void buildFeature(Feature feature) {
+        tamagotchi.setFeature(feature);
+    }
+
+    public void buildAccessory(Accessory accessory) {
+        tamagotchi.setAccessory(accessory);
+    }
+
+    public void buildPersonality(Personality personality) {
+        tamagotchi.setPersonality(personality);
+    }
+
+    // get the wedding package that was just built
+    public Tamagotchi getTamagotchi() {
+        return this.tamagotchi;
+    }
+
 }
